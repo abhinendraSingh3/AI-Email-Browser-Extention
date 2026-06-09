06a0b9b9a07fdc0326e8e9ec35510f170fed0a74
abhinendraSingh3 <abhinendrayadav3@gmail.com>
2026-06-10 00:00:39 +0530

    added service methods

--- email-writer-ai/src/main/java/com/email/email_writer_ai/service/EmailGeneratorService.java
+++ email-writer-ai/src/main/java/com/email/email_writer_ai/service/EmailGeneratorService.java
@@ -1,18 +1,46 @@
 package com.email.email_writer_ai.service;
-
 import com.email.email_writer_ai.EmailRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import java.util.Map;
 
 @Service
+@AllArgsConstructor
 public class EmailGeneratorService {
+
+    private final WebClient webClient;
+
+
+    //need api url
+//    @Value is a Spring annotation used to inject values into variables.
+//    Most commonly, it is used to read values from application.properties or application.yml.
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    //need api key
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
 
     public String generateEmailReply(EmailRequest emailRequest){
     //Build prompt
-    Sting Prompt=buildPrompt(emailRequest);
+    String prompt=buildPrompt(emailRequest);
 
     //craft a request--we need the structure like api defined
+        Map<String, Object> requestBody=Map.of(
+                "contents",new Object[]{
+                        Map.of("parts",new Object[]{
+                                Map.of("text",prompt)
+                        })
+                }
+        );
 
     //do request and get response
+        String response=webClient.post()
+                .uri(geminiApiUrl+geminiApiKey)
+                .
+
     //return response
 
 
